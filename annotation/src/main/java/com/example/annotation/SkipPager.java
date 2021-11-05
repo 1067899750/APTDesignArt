9d32a29b22553e3747e3f0d14b21e54527b1a64a
p_yantaopu <zm9523ZMC>
2021-11-05 14:56:48 +0800

    提交注解

--- annotation/src/main/java/com/example/annotation/SkipPager.java
+++ annotation/src/main/java/com/example/annotation/SkipPager.java
@@ -5,8 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @Desc:
+ * @Author: puyantao
+ * @CreateDate: 2021/11/5 14:55
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface BindView {
+public @interface SkipPager {
     String value();
 }
